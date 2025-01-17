b34cdf12cb01f8c838fd07366251695d48d30eea
RBLucky <nkosi.s.lucky@gmail.com>
2025-01-17 09:19:33 +0200

    connect movie service to deployed backend not localhost

--- src/services/movies.js
+++ src/services/movies.js
@@ -1,37 +1,37 @@
 import axios from "axios";
 
 class MovieDataService {
-  getAll(page = 0) {
-    return axios.get(`http://localhost:5000/api/v1/movies?page=${page}`);
-  }
-
-  get(id) {
-    return axios.get(`http://localhost:5000/api/v1/movies/id/${id}`);
-  }
-
-  find(query, by = "title", page = 0, rating) {
-    return axios.get(
-      `http://localhost:5000/api/v1/movies?${by}=${query}&page=${page}`
-    );
-  }
-
-  createReview(data) {
-    return axios.post("http://localhost:5000/api/v1/movies/review", data);
-  }
-
-  updateReview(data) {
-    return axios.put("http://localhost:5000/api/v1/movies/review", data);
-  }
-
-  deleteReview(id, userId) {
-    return axios.delete("http://localhost:5000/api/v1/movies/review", {
-      data: { review_id: id, user_id: userId },
-    });
-  }
-
-  getRatings() {
-    return axios.get("http://localhost:5000/api/v1/movies/ratings");
-  }
+    getAll(page = 0) {
+        return axios.get(`https://movie-backend-17nq.onrender.com/api/v1/movies?page=${page}`)
+    }
+    get(id) {
+        return axios.get(`https://movie-backend-17nq.onrender.com/api/v1/movies/id/${id}`)
+    }
+    // find(title) {
+    //     return axios.get(
+    //         `http://localhost:5000/api/v1/movies?title=${title}`
+    //     )
+    // }
+    find(query, by = "title", page = 0, rating) {
+        return axios.get(
+            `https://movie-backend-17nq.onrender.com/api/v1/movies?${by}=${query}&page=${page}&rating=${rating}`
+        )
+    }
+    createReview(data) {
+        return axios.post("https://movie-backend-17nq.onrender.com/api/v1/movies/review", data)
+    }
+    updateReview(data) {
+        return axios.put("https://movie-backend-17nq.onrender.com/api/v1/movies/review", data)
+    }
+    deleteReview(id, userId) {
+        return axios.delete(
+            "https://movie-backend-17nq.onrender.com/api/v1/movies/review",
+            { data: { review_id: id, user_id: userId } }
+        )
+    }
+    getRatings() {
+        return axios.get("https://movie-backend-17nq.onrender.com/api/v1/movies/ratings")
+    }
 }
 
 const movieDataService = new MovieDataService();
